deed00420068244c80f8afe83199b9022935b103
ntan1902 <trinh.an.1902@gmail.com>
2021-06-24 20:26:22 +0700

    Fetching data from Frontend

--- frontend/src/components/feed/Feed.jsx
+++ frontend/src/components/feed/Feed.jsx
@@ -1,19 +1,33 @@
 import Share from "../share/Share";
 import Post from "../post/Post";
 import "./feed.css";
-import { Posts } from "../../dummyData";
+import {useEffect, useState} from "react";
+import axios from "axios";
 
 function Feed() {
-  return (
-    <div className="feed">
-      <div className="feedWrapper">
-        <Share />
-        {Posts.map((post) => (
-          <Post key={post.id} post={post} />
-        ))}
-      </div>
-    </div>
-  );
+    const [posts, setPosts] = useState([])
+
+    // Run this once
+    useEffect(() => {
+        const fetchPosts = async () => {
+            const res = await axios.get("/users/1/all-posts")
+            console.log(res.data)
+            setPosts(res.data)
+        }
+
+        fetchPosts();
+
+    }, [])
+    return (
+        <div className="feed">
+            <div className="feedWrapper">
+                <Share/>
+                {posts.map(post => (
+                  <Post key={post.data.id} post={post} />
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default Feed;
--- frontend/src/components/post/Post.jsx
+++ frontend/src/components/post/Post.jsx
@@ -1,70 +1,82 @@
-import { MoreVert } from "@material-ui/icons";
-import { useState } from "react";
+import {MoreVert} from "@material-ui/icons";
+import {useEffect, useState} from "react";
 import "./post.css";
-import { Users } from "../../dummyData";
+import axios from "axios";
+import {format} from "timeago.js"
 
-function Post({ post }) {
-  const [like, setLike] = useState(post.like);
-  const [isLiked, setIsLiked] = useState(false);
+function Post({post}) {
+    const [like, setLike] = useState(post.likes.length);
+    const [isLiked, setIsLiked] = useState(false);
+    const [user, setUser] = useState({});
 
-  const PF = process.env.REACT_APP_PUBLIC_FOLDER;
+    const PF = process.env.REACT_APP_PUBLIC_FOLDER;
 
-  const likeHandler = () => {
-    setLike(isLiked ? like - 1 : like + 1);
-    setIsLiked(!isLiked);
-  };
+    useEffect(() => {
+        const fetchUser = async () => {
+            const res = await axios.get(`/users/${post.data.userId}`)
+            setUser(res.data)
+        }
+        fetchUser();
+    }, [post.data.userId])
 
-  return (
-    <div className="post">
-      <div className="postWrapper">
-        <div className="postTop">
-          <div className="postTopLeft">
-            <img
-              className="postProfileImg"
-              src={
-                Users.filter((user) => user.id === post.userId)[0]
-                  .profilePicture
-              }
-              alt=""
-            />
-            <span className="postUsername">
-              {Users.filter((user) => user.id === post.userId)[0].username}
-            </span>
-            <span className="postDate">{post.date}</span>
-          </div>
+    const likeHandler = () => {
+        setLike(isLiked ? like - 1 : like + 1);
+        setIsLiked(!isLiked);
+    };
 
-          <div className="postTopRight">
-            <MoreVert />
-          </div>
+    return (
+        <div className="post">
+            <div className="postWrapper">
+                <div className="postTop">
+                    <div className="postTopLeft">
+                        <img
+                            className="postProfileImg"
+                            src={
+                                user.profilePicture || PF + "person/noAvatar.png"
+                            }
+                            alt=""
+                        />
+                        <span className="postUsername">
+                            {user.username}
+                        </span>
+                        <span className="postDate">
+                            {format(post.data.createdAt)}
+                        </span>
+                    </div>
+
+                    <div className="postTopRight">
+                        <MoreVert/>
+                    </div>
+                </div>
+
+                <div className="postCenter">
+                    <span className="postText">{post?.data.description}</span>
+                    <img className="postImg" src={post.data.img} alt=""/>
+                </div>
+
+                <div className="postBottom">
+                    <div className="postBottomLeft">
+                        <img
+                            className="likeIcon"
+                            src={`${PF}like.png`}
+                            onClick={likeHandler}
+                            alt=""
+                        />
+                        <img
+                            className="likeIcon"
+                            src={`${PF}heart.png`}
+                            onClick={likeHandler}
+                            alt=""
+                        />
+                        <span className="postLikeCounter">{like} people liked it</span>
+                    </div>
+                    <div className="postBottomRight">
+                        <span className="postCommentText">{post.data.comment} comments</span>
+                    </div>
+                </div>
+            </div>
         </div>
-
-        <div className="postCenter">
-          <span className="postText">{post?.desc}</span>
-          <img className="postImg" src={PF+post.photo} alt="" />
-        </div>
-
-        <div className="postBottom">
-          <div className="postBottomLeft">
-            <img
-              className="likeIcon"
-              src={`${PF}like.png`}
-              onClick={likeHandler}
-              alt=""
-            />
-            <img
-              className="likeIcon"
-              src={`${PF}heart.png`}
-              onClick={likeHandler}
-              alt=""
-            />
-            <span className="postLikeCounter">{like} people liked it</span>
-          </div>
-          <div className="postBottomRight">
-            <span className="postCommentText">{post.comment} comments</span>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+    );
 }
+
 export default Post;
